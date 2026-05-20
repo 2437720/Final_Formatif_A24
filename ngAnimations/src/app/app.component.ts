62db5442017dee5810c2499203392c4936c74088
Blanco German Alejandro <2437720@cegepmontpetit.ca>
2026-05-19 23:18:17 -0400

    PROGRES: Animation en boucle terminé

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -1,25 +1,26 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
 import { bounce, shakeX, tada } from 'ng-animate';
+import { delay } from 'rxjs';
 
 const ShakeAnimationTime = 2;
 const BounceAnimationTime = 4;
 const TadaAnimationTime = 3;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    animations: [
-      trigger('shake', 
-        [transition(':increment', useAnimation(shakeX, {
-          params: { timing: 2}
-        }))
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('shake',
+      [transition(':increment', useAnimation(shakeX, {
+        params: { timing: ShakeAnimationTime }
+      }))
       ]),
-      trigger('bounce', [transition(':increment', useAnimation(bounce, {params: { timing: BounceAnimationTime}}))]),
-      trigger('tada', [transition(':increment', useAnimation(tada, {params: {timing: TadaAnimationTime}}))])
-    ],
-    styleUrls: ['./app.component.css'],
-    standalone: true
+    trigger('bounce', [transition(':increment', useAnimation(bounce, { params: { timing: BounceAnimationTime } }))]),
+    trigger('tada', [transition(':increment', useAnimation(tada, { params: { timing: TadaAnimationTime } }))])
+  ],
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
 export class AppComponent {
   title = 'ngAnimations';
@@ -28,21 +29,44 @@ export class AppComponent {
   ng_bounce = 0;
   ng_tada = 0;
 
+
   constructor() {
   }
 
 
-  AnimerUneFois(){
+  AnimerUneFois(boucle?: boolean) {
     console.log("Animer une fois");
     this.ng_shake++;
-    
+
+
+
     setTimeout(() => {
       this.ng_bounce++;
+
+      setTimeout(() => {
+        this.ng_tada++;
+      }, TadaAnimationTime * 999);
+
     }, ShakeAnimationTime * 1000);
-    setTimeout(() => {      this.ng_tada++;
-    }, TadaAnimationTime * 1000);
-        
+
+
+    if(boucle){
+      this.animerEnBoucle(true);
+    }
+
+  }
+
+  animerEnBoucle(boucle: boolean) {
+    let timing = ShakeAnimationTime + BounceAnimationTime + TadaAnimationTime;
+
+      this.AnimerUneFois();
+      setTimeout(() => {
+        this.AnimerUneFois(true);
+
+      }, timing * 1000);
   
+
+
   }
 
 
